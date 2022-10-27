1df6d78b6d61d26a18ae99a0846bfda5b5d5bf0c
yorick.geoffre <yorick.geoffre@etu.uca.fr>
2022-10-27 20:27:28 +0200

    add files

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt upgrade -y
 sudo apt install -y git cmake g++ wget unzip build-essential ccache pkg-config
 
 #------------------------------------------------------------------------------------Hidapi
-sudo apt install -y libudev-dev libusb-1.0-0-dev
+sudo apt-get install -y libudev-dev libusb-1.0-0-dev
 mkdir hid
 cd hid
 git clone https://github.com/libusb/hidapi
@@ -20,21 +20,23 @@ sudo ldconfig
 #-------------------------------------------------------------------------------------Opencv
 cd ~/code/auto_installed/
 
-sudo apt install -y libtbb-dev ffmpeg freeglut3 freeglut3-dev binutils-gold libglew-dev mesa-common-dev libglew1.5-dev libglm-dev
+sudo apt-get install -y libtbb-dev ffmpeg freeglut3 freeglut3-dev binutils-gold libglew-dev mesa-common-dev libglm-dev libtbb-dev mesa-utils gtk2.0 libgtkglext1 libgtkglext1-dev libcanberra-gtk-module
+
+sudo apt-get install -y ffmpeg libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
 
 mkdir ocv
 cd ocv
 
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.x.zip
-unzip opencv.zip
-unzip opencv_contrib.zip
+unzip -o opencv.zip
+unzip -o opencv_contrib.zip
 
 rm opencv.zip
 rm opencv_contrib.zip
 
 mkdir -p build && cd build
-cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-4.x/modules ../opencv-4.x -DWITH_OPENGL=ON -DWITH_GTK=ON -DWITH_QT=OFF -DWITHTBB=ON
+cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-4.x/modules ../opencv-4.x -DWITH_FFMPEG=ON -DWITH_OPENGL=ON -DWITH_OPENCL=ON -DWITH_GTK=ON -DWITH_QT=OFF -DWITHTBB=ON
 
 cmake --build . -j7
 sudo make install
